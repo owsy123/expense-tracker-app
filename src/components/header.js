137f5e14b6cfdc563ee21dc55df0a08209eeee5b
owsy123 <owais.sohail3@gmail.com>
2020-06-19 20:10:54 +0500

    update on header just for checking

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <div>
-          <h1>  Expense Tracker</h1> 
+          <h1>  TRACK YOUR EXPENSE</h1> 
         </div>
     )
 }
